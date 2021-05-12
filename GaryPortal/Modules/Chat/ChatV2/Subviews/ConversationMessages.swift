090313c90db90801a7a422ae5d414f982442fd58
Stephanie R <irule2701@gmail.com>
2021-05-12 23:07:50 +0100

    feat(chat): Add conversationmessage file

---
+++ GaryPortal/Modules/Chat/ChatV2/Subviews/ConversationMessages.swift
@@ -0,0 +1,20 @@
+//
+//  ConversationMessageView.swift
+//  GaryPortal
+//
+//  Created by Tom Knighton on 12/05/2021.
+//
+
+import SwiftUI
+
+struct ConversationMessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConversationMessageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConversationMessageView()
+    }
+}
